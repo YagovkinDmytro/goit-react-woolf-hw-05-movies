73cc5210c039a29689dc42beb75a9e8a39ce4806
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-04-29 11:48:42 +0200

    pages

--- src/pages/MovieDetails/MoviesDetails.jsx
+++ src/pages/MovieDetails/MoviesDetails.jsx
@@ -5,24 +5,23 @@ const MoviesDetails = () => {
     <>
       <div>
         <img src="" alt="" />
-        <h2></h2>
-        <p></p>
-        <h3></h3>
-        <p></p>
-        <h4></h4>
-        <p></p>
+        <h2>Title 1</h2>
+        <p>text</p>
+        <h3>Title 2</h3>
+        <p>text</p>
+        <h4>Title 4</h4>
+        <p>text</p>
       </div>
       <div>
-        <h5>
-          <ul>
-            <li>
-              <NavLink to="/movies/:movieId/cast">Cast</NavLink>
-            </li>
-            <li>
-              <NavLink to="/movies/:movieId/reviews">Reviews</NavLink>
-            </li>
-          </ul>
-        </h5>
+        <h5>Title 5</h5>
+        <ul>
+          <li>
+            <NavLink to="/movies/:movieId/cast">Cast</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/:movieId/reviews">Reviews</NavLink>
+          </li>
+        </ul>
       </div>
     </>
   );
